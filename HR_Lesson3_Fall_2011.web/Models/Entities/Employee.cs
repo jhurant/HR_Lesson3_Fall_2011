d8eeb5ab385e5ea3a138dd82841c5079057ba5d4
Eric Katz <ericpkatz@gmail.com>
2013-10-28 19:07:57 -0400

    Added Employee Entity

---
+++ HR_Lesson3_Fall_2011.web/Models/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HR_Lesson3_Fall_2011.web.Models.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public DateTime DateHired { get; set; }
+    }
+}
