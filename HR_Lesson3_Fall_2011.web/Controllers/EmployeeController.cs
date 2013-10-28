53fda70930d9316debb3fa423836bcb358ed8281
Eric Katz <ericpkatz@gmail.com>
2013-10-28 19:03:44 -0400

    Added Employee List Page

---
+++ HR_Lesson3_Fall_2011.web/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HR_Lesson3_Fall_2011.web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+    }
+}
