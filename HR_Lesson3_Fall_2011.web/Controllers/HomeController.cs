22212120303531f392657a651620fe90a81d19d9
Eric Katz <ericpkatz@gmail.com>
2013-10-28 18:43:00 -0400

    Added Twitter Bootstrap

---
+++ HR_Lesson3_Fall_2011.web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HR_Lesson3_Fall_2011.web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
